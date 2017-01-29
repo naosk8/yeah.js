fc17482f350fd9fb27151261aa6344152ec952fc
naosk8 <keep.naoself.real@gmail.com>
2017-01-29 02:25:23 -0800

    added sample and refactoring

--- assets/yeah.js
+++ assets/yeah.js
@@ -4,7 +4,7 @@ var videoConstraints = {
     width: { min: 640, ideal: 1280, max: 1920 },
     height: { min: 400, ideal: 720, max: 1080 },
     facingMode: "user",
-    frameRate: { ideal: 20, max: 30 }
+    frameRate: { ideal: 10, max: 15 }
 };
 var chartOptions = {
     curveType: 'function',
@@ -36,29 +36,55 @@ window.onload = function() {
     videoElm.addEventListener('ended', function() {
         isVideoStopped = true;
     }, false);
+    videoElm.addEventListener('pause', function() {
+        isVideoStopped = true;
+    }, false);
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
+    var tmpCanvas = document.getElementById('tmp_canvas');
+    var tmpCtx = tmpCanvas.getContext('2d');
+    var tmpImageObject = new Image();
+
     var chartElm = document.getElementById('curve_chart');
     var chart = new google.visualization.LineChart(chartElm);
 
     var timeSeriesData = [];
-
     function drawChart(timeSeriesData) {
         if (chart) {
-            dataWithTitle = [['time', 'diff', 'Yeah']].concat(timeSeriesData);
-            vData = google.visualization.arrayToDataTable(dataWithTitle);
+            if (!vData) {
+                dataWithTitle = [['time', 'diff', 'Yeah']].concat(timeSeriesData);
+                vData = google.visualization.arrayToDataTable(dataWithTitle);
+            } else {
+                if (vData.getNumberOfRows() >= 101) {
+                    vData.removeRow(0);
+                }
+                vData.addRow(timeSeriesData[timeSeriesData.length - 1]);
+            }
             chart.draw(vData, chartOptions);
         }
     }
 
-    var doFindCnt = 0;
+    var getScaledImageData = function(context, scale) {
+        imageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
+        tmpImageObject.src = context.canvas.toDataURL();
+        tmpCanvas.width = context.canvas.width * scale;
+        tmpCanvas.height = context.canvas.height * scale;
+        tmpCtx.scale(1, 1);
+        tmpCtx.drawImage(tmpImageObject, 0, 0, tmpCanvas.width, tmpCanvas.height);
+        imageData = tmpCtx.getImageData(0, 0, tmpCanvas.width, tmpCanvas.height);
+        return imageData;
+    };
+
+    var scale, baseHeight = 128, doFindCnt = 0;
     var doFindFeatures = function(context) {
         if (isVideoStopped) {
             doFindCnt = 0;
+            lastCorners = void 0;
+            lastDescriptors = void 0;
             return;
         }
         doFindCnt++;
-        now = (new Date()).getTime() / 1000;
+        now = Math.round((new Date()).getTime() / 1000);
         if (!startTime) {
             startTime = now;
         }
@@ -66,9 +92,10 @@ window.onload = function() {
         height = context.canvas.height;
         width = context.canvas.width;
         tracking.Fast.THRESHOLD = 100 - datParam.sensitivity;
-        imageData = context.getImageData(0, 0, width, height);
-        gray = tracking.Image.grayscale(imageData.data, width, height);
-        corners = tracking.Fast.findCorners(gray, width, height);
+        scale = (baseHeight / height);
+        imageData = getScaledImageData(context, scale);
+        gray = tracking.Image.grayscale(imageData.data, tmpCanvas.width, tmpCanvas.height);
+        corners = tracking.Fast.findCorners(gray, tmpCanvas.width, tmpCanvas.height);
 
         if (datParam.autoAdjust) {
             if (corners.length > 400 && datParam.sensitivity > 0) {
@@ -86,14 +113,27 @@ window.onload = function() {
             }
         }
 
-        for (i = 0; i < corners.length; i += 2) {
-            context.fillStyle = '#f00';
-            context.fillRect(corners[i], corners[i + 1], 3, 3);
+        if (datParam.showCapturePanel) {
+            for (i = 0; i < corners.length; i += 2) {
+                context.fillStyle = '#f00';
+                context.fillRect(corners[i] * (1/scale), corners[i + 1] * (1/scale), 4, 4);
+    //            tmpCtx.fillStyle = '#f00';
+    //            tmpCtx.fillRect(corners[i], corners[i + 1], 3, 3);
+            }
         }
 
         descriptors = tracking.Brief.getDescriptors(gray, width, corners);
         if (lastDescriptors && lastCorners) {
-            matches = tracking.Brief.reciprocalMatch(lastCorners, lastDescriptors, corners, descriptors);
+            matches = tracking.Brief.match(lastCorners, lastDescriptors, corners, descriptors);
+            if (datParam.showCapturePanel) {
+                for (i = 0; i < matches.length; i += 2) {
+                    context.fillStyle = '#0f0';
+                    context.fillRect(matches[i].keypoint1[0] * (1/scale), matches[i].keypoint1[1] * (1/scale), 4, 4);
+                }
+            }
+
+            // 双方向のマッチ処理: 負荷が高いので一時カット
+            // matches = tracking.Brief.reciprocalMatch(lastCorners, lastDescriptors, corners, descriptors);
             diff = corners.length - matches.length;
             len = timeSeriesData.length;
             if (len >= 3) {
@@ -104,11 +144,7 @@ window.onload = function() {
             } else {
                 yeah = 0;
             }
-            timeSeriesData.push([
-                now - startTime,
-                diff,
-                yeah
-            ]);
+            timeSeriesData.push([now - startTime, diff, yeah]);
             if (len > 99) {
                 timeSeriesData = timeSeriesData.slice(len - 99);
             }
@@ -137,13 +173,17 @@ window.onload = function() {
         this.sensitivity = 50;
         this.autoAdjust = true;
         this.showCapturePanel = false;
-        this.modeCamera = function() {
+        this.playCamera = function() {
             videoElm.controls = false;
             videoElm.src = URL.createObjectURL(myStream);
         };
-        this.modeFile = function() {
+        this.playDance = function() {
             videoElm.controls = true;
             videoElm.src = "./assets/kazuhiro.mp4";
+        };
+        this.playGame = function() {
+            videoElm.controls = true;
+            videoElm.src = "./assets/umehara.mp4";
         };
     };
     var gui = new dat.GUI();
@@ -154,13 +194,14 @@ window.onload = function() {
     gui.add(datParam, 'autoAdjust');
     gui.add(datParam, 'showCapturePanel').onChange(function() {
         if (datParam.showCapturePanel) {
-            videoElm.style.opacity = 0;
+            videoElm.style.opacity = 0.5;
         } else {
             videoElm.style.opacity = 1;
         }
     });
-    gui.add(datParam, 'modeCamera');
-    gui.add(datParam, 'modeFile');
+    gui.add(datParam, 'playCamera');
+    gui.add(datParam, 'playDance');
+    gui.add(datParam, 'playGame');
 
     var colorList = ['pink', 'blue', 'yellow', 'green', 'orange', 'violet'];
     var magicLayer = document.getElementById('magic-layer');
@@ -169,7 +210,8 @@ window.onload = function() {
         value = Math.floor(value);
         var innerMagicLayer = document.createElement('div');
         innerMagicLayer.classList.add('inner-magic-layer');
-        cnt = Math.floor(value / 60);
+        cnt = Math.floor(value / 25);
+//        console.log(cnt);
         if (cnt == 0) {
             return;
         } else if (cnt < 5) {
@@ -208,31 +250,6 @@ window.onload = function() {
             innerMagicLayer.parentNode.removeChild(innerMagicLayer);
         }, captureInterval * 2);
     };
-
-    videos = document.querySelectorAll("video");
-    for (var i = 0, l = videos.length; i < l; i++) {
-        var video = videos[i];
-        var src = video.src || (function () {
-            var sources = video.querySelectorAll("source");
-            for (var j = 0, sl = sources.length; j < sl; j++) {
-                var source = sources[j];
-                var type = source.type;
-                var isMp4 = type.indexOf("mp4") != -1;
-                if (isMp4) return source.src;
-            }
-            return null;
-        })();
-        if (src) {
-            var isYoutube = src && src.match(/(?:youtu|youtube)(?:\.com|\.be)\/([\w\W]+)/i);
-            if (isYoutube) {
-                var id = isYoutube[1].match(/watch\?v=|[\w\W]+/gi);
-                id = (id.length > 1) ? id.splice(1) : id;
-                id = id.toString();
-                var mp4url = "http://www.youtubeinmp4.com/redirect.php?video=";
-                video.src = mp4url + id;
-            }
-        }
-    }
 
 }
 
