7f8fbbcce893bbb1feecdb3aa08a359723177bd4
naosk8 <keep.naoself.real@gmail.com>
2017-01-30 03:46:55 -0800

    replaced google chart to canvas-js

--- assets/yeah.js
+++ assets/yeah.js
@@ -1,26 +1,9 @@
-var captureInterval = 1000;
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 var videoConstraints = {
     width: { min: 640, ideal: 1280, max: 1920 },
     height: { min: 400, ideal: 720, max: 1080 },
     facingMode: "user",
     frameRate: { ideal: 10, max: 15 }
-};
-var chartOptions = {
-    curveType: 'function',
-    legend: {
-        position: 'bottom',
-        textStyle: {color: 'gray'}
-    },
-    width: 800,
-    height: 200,
-    lineWidth: 3,
-    colors:['darkorange', 'limegreen'],
-    backgroundColor: 'transparent', // 'black',
-    vAxis: {
-        viewWindow: {min: 0},
-        minValue: 0
-    }
 };
 function DatParam() {
     this.sensitivity = 60;
@@ -46,13 +29,14 @@ function DatParam() {
     };
 };
 var datParam = new DatParam();
-var i, z, y, height, width, imageData, gray, myStream, dataWithTitle, vData, yeah, len, scale,
-    matches, lastCorners, lastDescriptors, corners, descriptors, startTime, now, diff, videoElm, gui, canvas, ctx,
-    chartElm, chart, magicLayer;
-var isVideoStopped = false, baseHeight = 128, doFindCnt = 0, tsData = [];
+var i, z, y, height, width, imageData, gray, myStream, vData, yeah, tsDataSize, scale, scaleInverse,
+    matches, corners, descriptors, lastCorners, lastDescriptors, cornersSize, matchesSize,
+    startTime, now, diff, videoElm, gui, canvas, ctx, chart, magicLayer;
+var captureInterval = 1000, isVideoStopped = false, baseHeight = 128, doFindCnt = 0;
 var tmpCanvas = document.createElement('canvas');
 var tmpCtx = tmpCanvas.getContext('2d');
 
+var tableHeader = ['time', 'diff', 'Yeah'];
 var colorList = ['pink', 'blue', 'yellow', 'green', 'orange', 'violet'];
 var colorListLen = colorList.length;
 var innerMagicLayerTpl = document.createElement('div');
@@ -71,8 +55,7 @@ window.onload = function() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    chartElm = document.getElementById('curve_chart');
-    chart = new google.visualization.LineChart(chartElm);
+    initChart();
 
     magicLayer = document.getElementById('magic-layer');
 
@@ -92,6 +75,65 @@ window.onload = function() {
 
 }
 
+function initChart() {
+    chart = new CanvasJS.Chart("curve_chart", {
+        theme: "theme2",
+        zoomEnabled: false,
+        axisX: {
+            includeZero: false,
+            minimum: 0,
+            interval: 10
+        },
+        axisY: {
+            includeZero: false,
+            maximum: 100,
+            minimum: 0,
+            interval: 25,
+            gridColor: "silver",
+            tickColor: "silver"
+        },
+        backgroundColor: "transparent",
+        toolTip:{shared: true},
+        legend:{
+            fontColor: "silver",
+            verticalAlign: "bottom",
+            horizontalAlign: "center",
+            fontSize: 12,
+            fontFamily: "Lucida Sans Unicode",
+            cursor:"pointer",
+            itemclick : function(e) {
+                if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                    e.dataSeries.visible = false;
+                } else {
+                    e.dataSeries.visible = true;
+                }
+                chart.render();
+            }
+        },
+        data: [
+            {
+                type: "line",
+                markerType: "none",
+                lineThickness:3,
+                showInLegend: true,
+                name: "diff",
+                color: "darkorange",
+                dataPoints: []
+            },
+            {
+                type: "line",
+                markerType: "none",
+                lineThickness:3,
+                showInLegend: true,
+                name: "Yeah",
+                color: "limegreen",
+                dataPoints: []
+            }
+        ]
+    });
+    chart.render();
+};
+
 function turnOnVideo() {
     navigator.getUserMedia({audio: false, video: videoConstraints}, function(stream){
         myStream = stream;
@@ -102,22 +144,18 @@ function turnOnVideo() {
 
 function addEffect(value) {
     value = Math.floor(value);
-    var fragment = document.createDocumentFragment();
-    var innerMagicLayer = innerMagicLayerTpl.cloneNode();
     cnt = Math.floor(value / 5);
     if (cnt == 0) {
         return;
-    } else if (cnt < 5) {
-        particleSize = 'sm';
-    } else if (cnt < 10) {
-        particleSize = 'md';
-    } else {
-        particleSize = 'lg';
     }
-    cnt = (cnt > 12) ? 12 : cnt;
+    var fragment = document.createDocumentFragment();
+    var innerMagicLayer = innerMagicLayerTpl.cloneNode();
+    particleSize = getParticleSizeClass(cnt);
 
     b = bTpl.cloneNode();
     b.classList.add(particleSize);
+
+    cnt = (cnt > 12) ? 12 : cnt;
     var bList = [], index = 0;
     for (z = 0; z < cnt; z++) {
         bClone = b.cloneNode();
@@ -140,6 +178,16 @@ function addEffect(value) {
     }, captureInterval * 2);
 };
 
+function getParticleSizeClass(value) {
+    if (value < 5) {
+        return 'sm';
+    } else if (value < 10) {
+        return 'md';
+    } else {
+        return 'lg';
+    }
+}
+
 function findFeatures(context) {
     if (isVideoStopped) {
         doFindCnt = 0;
@@ -159,79 +207,85 @@ function findFeatures(context) {
     imageData = getScaledImageData();
     gray = tracking.Image.grayscale(imageData.data, imageData.width, imageData.height);
     corners = tracking.Fast.findCorners(gray, imageData.width, imageData.height);
+    cornersSize = corners.length;
 
-    if (datParam.autoAdjust) {
-        if (corners.length > 400 && datParam.sensitivity > 0) {
-            datParam.sensitivity -= Math.max(2, Math.ceil((corners.length - 400) / 500));
-            datParam.sensitivity = Math.max(0, datParam.sensitivity);
-            for (var i in gui.__controllers) {
-                gui.__controllers[i].updateDisplay();
-            }
-        } else if (corners.length < 250 && datParam.sensitivity < 100) {
-            datParam.sensitivity += Math.max(2, Math.ceil((250 - corners.length) / 100));
-            datParam.sensitivity = Math.min(100, datParam.sensitivity);
-            for (var i in gui.__controllers) {
-                gui.__controllers[i].updateDisplay();
-            }
-        }
-    }
+    autoAdjustSensitivity(datParam, cornersSize);
 
-    if (datParam.showCapturePanel) {
-        for (i = 0; i < corners.length; i += 2) {
-            context.fillStyle = '#f00';
-            context.fillRect(corners[i] * (1/scale), corners[i + 1] * (1/scale), 4, 4);
-        }
-    }
-
+    matches = [], matchesSize = 0;
     descriptors = tracking.Brief.getDescriptors(gray, width, corners);
-    if (corners.length > 0 && lastDescriptors && lastCorners) {
-        matches = tracking.Brief.match(lastCorners, lastDescriptors, corners, descriptors);
-        if (datParam.showCapturePanel) {
-            for (i = 0; i < matches.length; i += 2) {
-                context.fillStyle = '#0f0';
-                context.fillRect(matches[i].keypoint1[0] * (1/scale), matches[i].keypoint1[1] * (1/scale), 4, 4);
-            }
-        }
-
+    if (cornersSize > 0 && lastDescriptors && lastCorners) {
         // 双方向のマッチ処理: 負荷が高いので一時カット
         // matches = tracking.Brief.reciprocalMatch(lastCorners, lastDescriptors, corners, descriptors);
-        diff = (1 - Math.min(1, matches.length / corners.length)) * 100;
-        yeah = calcYeah(diff, tsData);
+        matches = tracking.Brief.match(lastCorners, lastDescriptors, corners, descriptors);
+        matchesSize = matches.length;
+        diff = (1 - Math.min(1, matchesSize / cornersSize)) * 100;
+        yeah = calcYeah(diff, chart.options.data[0].dataPoints);
         if (doFindCnt > 3) {
             addEffect(yeah);
         }
-        tsData.push([now - startTime, diff, yeah]);
-        drawChart(tsData);
+
+        appendChartData('diff', {x:now - startTime, y: diff});
+        appendChartData('Yeah', {x:now - startTime, y: yeah});
+        chart.render();
+    }
+
+    if (datParam.showCapturePanel) {
+        for (i = 0; i < cornersSize; i += 2) {
+            context.fillStyle = '#f00';
+            context.fillRect(corners[i] * scaleInverse, corners[i + 1] * scaleInverse, 4, 4);
+        }
+        for (i = 0; i < matchesSize; i += 2) {
+            context.fillStyle = '#0f0';
+            context.fillRect(matches[i].keypoint1[0] * scaleInverse, matches[i].keypoint1[1] * scaleInverse, 4, 4);
+        }
     }
 
     lastDescriptors = descriptors;
     lastCorners = corners;
 };
 
-function calcYeah(currentValue, tsData) {
-    len = tsData.length;
-    if (len >= 2) {
-        yeah = (Math.abs(currentValue - tsData[len-1][1]) * 2
-            + Math.abs(tsData[len-1][1] - tsData[len-2][1])) / 3;
+function autoAdjustSensitivity(datParam, targetValue) {
+    if (datParam.autoAdjust) {
+        if (targetValue > 400 && datParam.sensitivity > 0) {
+            datParam.sensitivity -= Math.max(2, Math.ceil((targetValue - 400) / 500));
+            datParam.sensitivity = Math.max(0, datParam.sensitivity);
+            updateDatParam();
+        } else if (targetValue < 250 && datParam.sensitivity < 100) {
+            datParam.sensitivity += Math.max(2, Math.ceil((250 - targetValue) / 100));
+            datParam.sensitivity = Math.min(100, datParam.sensitivity);
+            updateDatParam();
+        }
+    }
+}
+
+function updateDatParam() {
+    for (var i in gui.__controllers) {
+        gui.__controllers[i].updateDisplay();
+    }
+}
+
+function calcYeah(currentValue, dataPoints) {
+    tsDataSize = dataPoints.length;
+    if (tsDataSize >= 2) {
+        yeah = (Math.abs(currentValue - dataPoints[tsDataSize-1].y) * 2
+            + Math.abs(dataPoints[tsDataSize-1].y - dataPoints[tsDataSize-2].y)) / 3;
     } else {
         yeah = 0;
     }
     return yeah;
 }
 
-function drawChart(tsData) {
-    if (chart) {
-        if (!vData) {
-            dataWithTitle = [['time', 'diff', 'Yeah']].concat(tsData);
-            vData = google.visualization.arrayToDataTable(dataWithTitle);
-        } else {
-            if (vData.getNumberOfRows() >= 60) {
-                vData.removeRow(0);
-            }
-            vData.addRow(tsData[tsData.length - 1]);
-        }
-        chart.draw(vData, chartOptions);
+var dataKey;
+function appendChartData(type, rowData) {
+    if (type == 'diff') {
+        dataKey = 0;
+    } else if (type == 'Yeah') {
+        dataKey = 1;
     }
+    if (chart.options.data[dataKey].dataPoints.length >= 60) {
+        chart.options.data[dataKey].dataPoints.shift();
+    }
+    chart.options.data[dataKey].dataPoints.push(rowData);
 }
 
 function getScaledImageData() {
@@ -262,9 +316,14 @@ function initCanvasByVideo(delay) {
         canvas.width = videoElm.clientWidth;
         canvas.height = videoElm.clientHeight;
         scale = getScale(canvas.height);
+        scaleInverse = 1 / scale;
         tmpCanvas.width = canvas.width * scale;
         tmpCanvas.height = canvas.height * scale;
-        videoElm.style.opacity = 0.5;
+        if (datParam.showCapturePanel) {
+            videoElm.style.opacity = 0.5;
+        } else {
+            videoElm.style.opacity = 1;
+        }
         canvas.style.opacity = 1;
     }, delay);
 };
